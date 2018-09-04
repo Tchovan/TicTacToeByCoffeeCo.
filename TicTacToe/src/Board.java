0fff3bdb31b19e32cfeb4b849e11a76324d2934e
Tchovan <Tchovan@luc.edu>
2018-09-04 18:16:26 -0500

    Created empty methods and outlines of classes based on Phil's PDFs

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -1,4 +1,15 @@
 //Class that handles data regarding the board.
 
 public class Board {
+
+    Player getPlayerPosition(int[] position) {      //returns players given position
+    }
+
+    boolean isFull() {      //checks to see if the board is full
+    }
+
+    boolean move(Player player, int[] position) {   //player makes a move on the board
+    }
+
+
 }
